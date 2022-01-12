41540732fbc0164a9beae25bafbc5d1bcdbb6714
Kwabena Agyeman <kagyeman@ymail.com>
2022-01-12 10:23:46 +0000

    lesson 4

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,13 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <motion.div animate={{}} className='home container'>
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      animate={{ opacity: 1 }}
+      className='home container'
+    >
       <motion.h2>Welcome to Pizza Joint</motion.h2>
       <Link to='/base'>
         <motion.button animate={{}}>Create Your Pizza</motion.button>
